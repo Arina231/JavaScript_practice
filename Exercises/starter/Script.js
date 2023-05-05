b289103b608ef3e632840ae9c0439cf4e121f5cd
Arina231 <arinahonaa119@gmail.com>
2023-05-06 00:51:04 +0100

    practice

--- Exercises/starter/Script.js
+++ Exercises/starter/Script.js
@@ -2,4 +2,8 @@ let js = "amazing";
 console.log(40 + 48 + 10);
 
 let firstName = "ahona";
-console.log(firstN ame);
+console.log(firstName);
+
+const ageCurrent = 2052 - 1999;
+const ageYear = 2052 - 1900;
+console.log(ageCurrent, ageYear, 9 ** 3);
