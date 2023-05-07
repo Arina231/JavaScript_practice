4b6c0493588ebae5f29c8cd326b9690876de7bc5
Arina231 <arinahonaa119@gmail.com>
2023-05-07 22:35:40 +0100

    practice

--- Exercises/starter/Script.js
+++ Exercises/starter/Script.js
@@ -7,3 +7,9 @@ console.log(firstName);
 const ageCurrent = 2052 - 1999;
 const ageYear = 2052 - 1900;
 console.log(ageCurrent, ageYear);
+
+const age = 18;
+const isOldEnough = age >= 18;
+if (isOldEnough) {
+  console.log("Sarah can drive");
+}
