6b508f10b9bc2ba414bc139651ebc7ab241b727e
Arina231 <arinahonaa119@gmail.com>
2023-05-16 22:50:15 +0100

    lect25cc4

--- Exercises/starter/Script.js
+++ Exercises/starter/Script.js
@@ -1,36 +1,48 @@
-let js = "amazing";
-console.log(40 + 48 + 10);
+// let js = "amazing";
+// console.log(40 + 48 + 10);
 
-let firstName = "ahona";
-console.log(firstName);
+// let firstName = "ahona";
+// console.log(firstName);
 
-const ageCurrent = 2052 - 1999;
-const ageYear = 2052 - 1900;
-console.log(ageCurrent, ageYear);
+// const ageCurrent = 2052 - 1999;
+// const ageYear = 2052 - 1900;
+// console.log(ageCurrent, ageYear);
 
-const age = 12;
+// const age = 12;
 
-if (age >= 18) {
-  console.log("Sarah can drive");
-} else {
-  const yearsLeft = 18 - age;
-  console.log(`wait another ${yearsLeft} years.`);
-}
+// if (age >= 18) {
+//   console.log("Sarah can drive");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`wait another ${yearsLeft} years.`);
+// }
 
-// const MarksMass = 95;
-// const MarksHeight = 1.88;
-// const JohnsMass = 85;
-// const JohnsHeight = 1.76;
+// // const MarksMass = 95;
+// // const MarksHeight = 1.88;
+// // const JohnsMass = 85;
+// // const JohnsHeight = 1.76;
 
-const MarksMass = 78;
-const MarksHeight = 1.69;
-const JohnsMass = 92;
-const JohnsHeight = 1.95;
+// const MarksMass = 78;
+// const MarksHeight = 1.69;
+// const JohnsMass = 92;
+// const JohnsHeight = 1.95;
 
-const MarksBMI = MarksMass / MarksHeight ** 2;
-const JohnsBMI = JohnsMass / JohnsHeight ** 2;
-if (MarksBMI < JohnsBMI) {
-  console.log(`John's BMI ${JohnsBMI} is higher than Mark's BMI ${MarksBMI}`);
-} else {
-  console.log(`Mark's BMI ${MarksBMI} is higher than John's BMI ${JohnsBMI}`);
-}
+// const MarksBMI = MarksMass / MarksHeight ** 2;
+// const JohnsBMI = JohnsMass / JohnsHeight ** 2;
+// if (MarksBMI < JohnsBMI) {
+//   console.log(`John's BMI ${JohnsBMI} is higher than Mark's BMI ${MarksBMI}`);
+// } else {
+//   console.log(`Mark's BMI ${MarksBMI} is higher than John's BMI ${JohnsBMI}`);
+// }
+
+const age = 24;
+age >= 18 ? console.log("wine please") : console.log("water.");
+
+const drink = age >= 18 ? console.log("wine please") : console.log("water.");
+
+const Bill = 352;
+const Tip = Bill <= 300 && Bill >= 50 ? Bill * 0.15 : Bill * 0.2;
+
+console.log(
+  `The bill was ${Bill},the tip was ${Tip}, and the total value ${Bill + Tip}`
+);
