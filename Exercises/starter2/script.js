fb14d32363554cb1b11e5a47c30d558168de2343
Arina231 <arinahonaa119@gmail.com>
2023-05-17 23:42:15 +0100

    modified:   Exercises/starter2/script.js

--- Exercises/starter2/script.js
+++ Exercises/starter2/script.js
@@ -1,1 +1,7 @@
-"use strict";
+// "use strict";
+function fruitprocessor(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitprocessor(6, 0);
+console.log(appleJuice);
