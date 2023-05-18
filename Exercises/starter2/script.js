ca2228d955fd43ba4ba9fff366dc7dc96d3c1a4a
Arina231 <arinahonaa119@gmail.com>
2023-05-18 23:10:46 +0100

    modified:   Exercises/starter2/script.js

--- Exercises/starter2/script.js
+++ Exercises/starter2/script.js
@@ -3,8 +3,7 @@ function fruitprocessor(apples, oranges) {
   const juice = `juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
-const appleJuice = fruitprocessor(3, 3);
-console.log(appleJuice);
+
 const appleOrangeJuice = fruitprocessor(3, 3);
 console.log(appleOrangeJuice);
 
