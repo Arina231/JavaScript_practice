adebb3e116876fc64db10825d428f186e0901715
Arina231 <arinahonaa119@gmail.com>
2023-05-20 22:38:43 +0100

    modified:   Exercises/starter2/script.js

--- Exercises/starter2/script.js
+++ Exercises/starter2/script.js
@@ -1,28 +1,32 @@
 // "use strict";
-function fruitprocessor(apples, oranges) {
-  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
+// function fruitprocessor(apples, oranges) {
+//   const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
 
-const appleOrangeJuice = fruitprocessor(3, 3);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitprocessor(3, 3);
+// console.log(appleOrangeJuice);
 
-//function expression
-const fruitprocessor2 = function (apples, oranges) {
-  return `juice with ${apples} apples and ${oranges} oranges.`;
+// //function expression
+// const fruitprocessor2 = function (apples, oranges) {
+//   return `juice with ${apples} apples and ${oranges} oranges.`;
+// };
+// const appleJuice = fruitprocessor2(3, 3);
+// console.log(appleJuice);
+
+// //arrow function
+
+// const fruitprocessor3 = (apples, oranges) => {
+//   return `juice with ${apples} apples and ${oranges} oranges.`;
+// };
+
+// console.log(fruitprocessor3(3, 3));
+
+// const calPrice = (perKgP, weight) => {
+//   return `total price is ${perKgP * weight}`;
+// };
+// console.log(calPrice(30, 3));
+
+const calcAverage = (X, Y, Z) => {
+  return `the average is ${(X + Y + Z) / 3}`;
 };
-const appleJuice = fruitprocessor2(3, 3);
-console.log(appleJuice);
-
-//arrow function
-
-const fruitprocessor3 = (apples, oranges) => {
-  return `juice with ${apples} apples and ${oranges} oranges.`;
-};
-
-console.log(fruitprocessor3(3, 3));
-
-const calPrice = (perKgP, weight) => {
-  return `total price is ${perKgP * weight}`;
-};
-console.log(calPrice(30, 3));
