65f2e3c63787238963b4cd46f1e0b587aecaf25c
Arina231 <arinahonaa119@gmail.com>
2023-05-20 23:30:49 +0100

    modified:   Exercises/starter2/script.js

--- Exercises/starter2/script.js
+++ Exercises/starter2/script.js
@@ -36,3 +36,14 @@ const scoreDolphin = calcAverage(44, 23, 71);
 const scoreKoalas = calcAverage(65, 54, 49);
 
 console.log(scoreDolphin, scoreKoalas);
+
+const checkWinner = (scoreDolphin, scoreKoalas) => {
+  if (scoreDolphin >= 2 * scoreKoalas) {
+    console.log`Dolphin win ${scoreDolphin} vs ${scoreKoalas}`;
+  } else if (scoreKoalas >= 2 * scoreDolphin) {
+    console.log`Koalas win ${scoreKoalas} vs ${scoreDolphin}`;
+  } else {
+    console.log("no team wins");
+  }
+};
+checkWinner(scoreDolphin, scoreKoalas);
