954aad9ade8c2b985f50c24084dbd67f97a8cb8d
Arina231 <arinahonaa119@gmail.com>
2023-06-11 00:22:31 +0100

    modified:   Exercises/starter3/script.js

--- Exercises/starter3/script.js
+++ Exercises/starter3/script.js
@@ -6,3 +6,17 @@ if (x === 24) {
   console.log(24);
 }
 const calcAge = birthYear => 2037 - birthYear;
+
+const tempp = [3, -2, -1, -6, 'error', 15, 11, 10, 18];
+const calcTempsAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (typeof currentTemp !== 'number') continue;
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  console.log(max, min);
+};
+calcTempsAmplitude(tempp);
