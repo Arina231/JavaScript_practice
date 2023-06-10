f121d61596a71262cb65783d390cead9c7c23d45
Arina231 <arinahonaa119@gmail.com>
2023-06-10 20:29:30 +0100

    new file:   Exercises/starter3/.prettierrc 	modified:   Exercises/starter3/script.js

--- Exercises/starter3/script.js
+++ Exercises/starter3/script.js
@@ -1,2 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+'use strict';
+
+const x = '24';
+if (x === 24) {
+  console.log(24);
+}
+const calcAge = birthYear => 2037 - birthYear;
