e2c72b52a007321c0047368d3b94612c1271438d
Arina231 <arinahonaa119@gmail.com>
2023-06-12 22:09:10 +0100

    modified:   Exercises/starter3/script.js

--- Exercises/starter3/script.js
+++ Exercises/starter3/script.js
@@ -21,12 +21,13 @@
 // };
 // calcTempsAmplitude(tempp);
 
-const measureKelvin = function{
+const measureKelvin = function () {
   const measurement = {
-    type :'temp',
-    unit:'celsius',
-    value:Number (prompt('degree celsius:'))
-  }
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('degree celsius:')),
+  };
   const kelvin = measurement.value + 273;
   return kelvin;
-}
+};
+console.log(measureKelvin());
