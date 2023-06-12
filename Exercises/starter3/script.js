5f58c7dd862b807a9cc7cdc786f4c317aec82c07
Arina231 <arinahonaa119@gmail.com>
2023-06-12 21:42:28 +0100

    modified:   Exercises/starter3/script.js

--- Exercises/starter3/script.js
+++ Exercises/starter3/script.js
@@ -1,22 +1,32 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '24';
-if (x === 24) {
-  console.log(24);
-}
-const calcAge = birthYear => 2037 - birthYear;
+// const x = '24';
+// if (x === 24) {
+//   console.log(24);
+// }
+// const calcAge = birthYear => 2037 - birthYear;
 
-const tempp = [3, -2, -1, -6, 'error', 15, 11, 10, 18];
-const calcTempsAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
-  for (let i = 0; i < temps.length; i++) {
-    const currentTemp = temps[i];
-    if (typeof currentTemp !== 'number') continue;
-    if (temps[i] > max) max = temps[i];
-    if (temps[i] < min) min = temps[i];
+// const tempp = [3, -2, -1, -6, ' ', 15, 11, 10, 18];
+// const calcTempsAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const currentTemp = temps[i];
+//     if (typeof currentTemp !== 'number') continue;
+//     if (temps[i] > max) max = temps[i];
+//     if (temps[i] < min) min = temps[i];
+//   }
+//   console.log(max, min);
+// };
+// calcTempsAmplitude(tempp);
+
+const measureKelvin = function{
+  const measurement = {
+    type :'temp',
+    unit:'celsius',
+    value:Number (prompt('degree celsius:'))
   }
-  console.log(max, min);
-};
-calcTempsAmplitude(tempp);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+}
